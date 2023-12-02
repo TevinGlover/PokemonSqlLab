84ab249ae2640b5fb3eb06e743af61c55cf8010d
Tevin Glover <tevwork.tg@gmail.com>
2023-12-02 12:58:43 -0500

    start

---
+++ answers/Part two.sql
@@ -0,0 +1,10 @@
+-- Question1
+SELECT * FROM types;
+-- Question2
+ SELECT * FROM pokemons LIMITS 45;
+-- QUSETION 3
+SELECT COUNT (*)FROM POKEMONS;
+-- QUESTION 4
+ SELECT COUNT(*) FROM  Types;
+ -- Question 5
+  SELECT COUNT(*) FROM pokemons WHERE  secondary_type;
